b739b8452d039b0aeca2f9953b382a24d64ad131
kikishula-web <kikishula@gmail.com>
2025-10-27 14:20:49 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // >>> EDITA ESTO <<<
-const WHATSAPP_PHONE = "15551234567"; // solo números con lada (ej: 1 para USA)
+const WHATSAPP_PHONE = "19209445309"; // solo números con lada (ej: 1 para USA)
 const WHATSAPP_MSG   = "Hola, quiero ordenar Puravida Fit 🍍";
 const PAYPAL_LINK    = "https://www.paypal.me/tuusuario/35"; // o link a Shopify/Stripe
 
